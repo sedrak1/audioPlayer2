20a736cf386c670eca9b34e5f9a0731766c2753a
Sedrak Mkrtchyan <setmk777@gmail.com>
2021-12-19 15:50:11 +0400

    push code

--- Buttons/Buttons.js
+++ Buttons/Buttons.js
@@ -1,78 +1,105 @@
 export default class Buttons {
-    
-    constructor( songs, currentSong){
-
-        this.play = document.createElement("button")
-        this.pause = document.createElement("button")
-        this.prev = document.createElement("button")
-        this.next = document.createElement("button")
-        this.playing = false
-        this.parent = document.createElement("div")
-        this.songs = songs
-        this.currentSong = currentSong
-        this.play.onclick = () => this.playSong()
-        this.pause.onclick = () => this.pauseSong()
-        this.prev.onclick = () => this.prevSong()
-        this.next.onclick = () => this.nextSong()
-        this.parent.append(this.play,this.pause,this.prev,this.next)
-        document.body.append(this.parent)
+    constructor(songs, currentSong, playing) {
+        this.songs = songs;
+        this.currentSong = currentSong;
+        this.playing = playing;
+        this.play = document.createElement("button");
+        this.pause = document.createElement("button");
+        this.prev = document.createElement("button");
+        this.next = document.createElement("button");
+        this.playing = false;
+        this.parent = document.createElement("div");
+        this.play.onclick = () => this.playSong();
+        this.pause.onclick = () => this.pauseSong();
+        this.prev.onclick = () => this.prevSong();
+        this.next.onclick = () => this.nextSong();
+        this.parent.append(this.play, this.pause, this.prev, this.next);
+        document.body.append(this.parent);
     }
 
     playSong() {
-        if(this.playing){
-            audio.pause()
-            let i = document.createElement("i")
-            i.className = "fa fa-play-circle-o"
-            this.play.appendChild(i)
-            this.playing = !this.playing
+        if (this.playing) {
+            this.pause.style.display = "none";
+            audio.pause();
+            let i = document.createElement("i");
+            i.className = "fa fa-play-circle-o";
+            this.play.appendChild(i);
+            this.playing = !this.playing;
         }
     }
 
-    pauseSong(){
-        if(!this.playing){
-            audio.play()
-            let i = document.createElement("i")
-            i.className = "fa fa-pause-circle-o"
-            this.pause.appendChild(i)
-            this.playing = !this.playing
+    pauseSong() {
+        if (!this.playing) {
+            this.play.style.display = "none";
+            audio.play();
+            let i = document.createElement("i");
+            i.className = "fa fa-pause-circle-o";
+            this.pause.appendChild(i);
+            this.playing = !this.playing;
         }
     }
 
-    nextSong(){
-        this.enableBtn("prev")
+    nextSong() {
+        this.enableBtn(this.prev);
 
-        if(this.currentSong === this.songs.length - 2){
-            this.disableBtn("next")
+        if (this.currentSong === this.songs.length - 2) {
+            this.disableBtn(this.next);
         }
-        
-        audio.remove()
-        this.createAudio(("http://localhost/audioPlayer/music/" + this.songs[this.currentSong + 1]).split(" ").join("%20"),this.songs[this.currentSong + 1])
 
-        this.getCurrentSongDuration()
-        
-        if(this.playing){
-            audio.play()
+        audio.remove();
+        this.createAudio(
+            (
+                "http://localhost/audioPlayer/music/" +
+                this.songs[this.currentSong + 1]
+            )
+                .split(" ")
+                .join("%20"),
+            this.songs[this.currentSong + 1]
+        );
+
+        this.getCurrentSongDuration();
+
+        if (this.playing) {
+            audio.play();
         }
-        this.currentSong ++     
-        
+        this.currentSong++;
     }
 
-    prevSong(){
-        this.enableBtn("next")
-        fetch("http://localhost/audioPlayer/data.json").then((response) => response.json()).then((data) => {
-            if(this.currentSong <= 1){
-                this.disableBtn("prev")
-            }
-            
-            audio.remove()
-            this.createAudio(("http://localhost/audioPlayer/music/" + data[this.currentSong - 1]).split(" ").join("%20"),data[this.currentSong - 1])
+    prevSong() {
+        this.enableBtn(this.next);
+        fetch("http://localhost/audioPlayer/data.json")
+            .then((response) => response.json())
+            .then((data) => {
+                if (this.currentSong <= 1) {
+                    this.disableBtn(this.prev);
+                }
 
-            this.getCurrentSongDuration()
-            
-            if(this.playing){
-                audio.play()
-            }
-            this.currentSong -- 
-        })
-    }    
-}   
+                audio.remove();
+                this.createAudio(
+                    (
+                        "http://localhost/audioPlayer/music/" +
+                        data[this.currentSong - 1]
+                    )
+                        .split(" ")
+                        .join("%20"),
+                    data[this.currentSong - 1]
+                );
+
+                this.getCurrentSongDuration();
+
+                if (this.playing) {
+                    audio.play();
+                }
+                this.currentSong--;
+            });
+    }
+    disableBtn(btn) {
+        btn.disabled = true;
+        btn.style.color = "#2121216c";
+    }
+
+    enableBtn(btn) {
+        btn.disabled = false;
+        btn.style.color = "#5c5c5c";
+    }
+}
--- index.js
+++ index.js
@@ -1,182 +1,216 @@
-import Buttons from "./Buttons/Buttons.js"
+import Buttons from "./Buttons/Buttons.js";
 
-class Player{
-    constructor(){
-        this.mouseDown=false
-    }
-    currentSong = 0
-    playing = false
-    currentSongDuration = 0
-
-    playPause() {
-        if(this.playing){
-            document.getElementById("music").pause()
-            document.getElementById("playPause").textContent=""
-            let i = document.createElement("i")
-            i.className = "fa fa-play-circle-o"
-            document.getElementById("playPause").appendChild(i)
-            this.playing = !this.playing
-        }else{
-            document.getElementById("music").play()
-            document.getElementById("playPause").textContent=""
-            let i = document.createElement("i")
-            i.className = "fa fa-pause-circle-o"
-            document.getElementById("playPause").appendChild(i)
-            this.playing = !this.playing
-
-        }
+class Player {
+    constructor() {
+        this.mouseDown = false;
+        this.currentSong = 0;
+        this.playing = false;
+        this.currentSongDuration = 0;
+        this.songs = []
         
     }
-
-    createAudio(song,songName){
-        if(document.getElementById("songName")){
-            document.getElementById("songName").remove()
-        }
-        
-        let parent = document.getElementById("player")
-        let audio = document.createElement("audio")
-        let source = document.createElement("source")
-        let name = document.createElement("p")
-
-        name.id = "songName"
-        audio.id="music"
-        audio.preload = "metadata"
-        source.id="source"
-        parent.insertBefore(audio, parent.children[1])
-        audio.appendChild(source)
-        source.src = song
-        audio.onplay = () => { this.playing = true }
     
-        name.textContent = songName.substring(0, songName.length - 4)
-        document.getElementById("songInfo").appendChild(name)
+    playPause() {
+        this.songs = fetch("http://localhost/audioPlayer/data.json")
+        .then((response) => response.json()).then((data) => (this.songs = data))
+        console.log(this.songs);
+        if (this.playing) {
+            document.getElementById("music").pause();
+            document.getElementById("playPause").textContent = "";
+            let i = document.createElement("i");
+            i.className = "fa fa-play-circle-o";
+            document.getElementById("playPause").appendChild(i);
+            this.playing = !this.playing;
+        } else {
+            document.getElementById("music").play();
+            document.getElementById("playPause").textContent = "";
+            let i = document.createElement("i");
+            i.className = "fa fa-pause-circle-o";
+            document.getElementById("playPause").appendChild(i);
+            this.playing = !this.playing;
+        }
     }
 
-    nextSong(){
-        this.enableBtn("prev")
-        
-        fetch("http://localhost/audioPlayer/data.json").then((response) => response.json()).then((data) => {
-            if(this.currentSong === Object.keys(data).length - 2){
-                this.disableBtn("next")
-            }
-            
-            document.getElementById("music").remove()
-            this.createAudio(("http://localhost/audioPlayer/music/" + data[this.currentSong + 1]).split(" ").join("%20"),data[this.currentSong + 1])
+    createAudio(song, songName) {
+        if (document.getElementById("songName")) {
+            document.getElementById("songName").remove();
+        }
 
-            this.getCurrentSongDuration()
-            
-            if(this.playing){
-                document.getElementById("music").play()
-            }
-            this.currentSong ++     
-        })
+        let parent = document.getElementById("player");
+        let audio = document.createElement("audio");
+        let source = document.createElement("source");
+        let name = document.createElement("p");
+
+        name.id = "songName";
+        audio.id = "music";
+        audio.preload = "metadata";
+        source.id = "source";
+        parent.insertBefore(audio, parent.children[1]);
+        audio.appendChild(source);
+        source.src = song;
+        audio.onplay = () => {
+            this.playing = true;
+        };
+
+        name.textContent = songName.substring(0, songName.length - 4);
+        document.getElementById("songInfo").appendChild(name);
     }
 
-    prevSong(){
-        this.enableBtn("next")
-        fetch("http://localhost/audioPlayer/data.json").then((response) => response.json()).then((data) => {
-            if(this.currentSong <= 1){
-                this.disableBtn("prev")
-            }
-            
-            document.getElementById("music").remove()
-            this.createAudio(("http://localhost/audioPlayer/music/" + data[this.currentSong - 1]).split(" ").join("%20"),data[this.currentSong - 1])
+    nextSong() {
+        this.enableBtn("prev");
 
-            this.getCurrentSongDuration()
-            
-            if(this.playing){
-                document.getElementById("music").play()
-            }
-            this.currentSong -- 
-        })
+        fetch("http://localhost/audioPlayer/data.json")
+            .then((response) => response.json())
+            .then((data) => {
+                if (this.currentSong === Object.keys(data).length - 2) {
+                    this.disableBtn("next");
+                }
+
+                document.getElementById("music").remove();
+                this.createAudio(
+                    (
+                        "http://localhost/audioPlayer/music/" +
+                        data[this.currentSong + 1]
+                    )
+                        .split(" ")
+                        .join("%20"),
+                    data[this.currentSong + 1]
+                );
+
+                this.getCurrentSongDuration();
+
+                if (this.playing) {
+                    document.getElementById("music").play();
+                }
+                this.currentSong++;
+            });
     }
 
-    getCurrentSongDuration(){
+    prevSong() {
+        this.enableBtn("next");
+        fetch("http://localhost/audioPlayer/data.json")
+            .then((response) => response.json())
+            .then((data) => {
+                if (this.currentSong <= 1) {
+                    this.disableBtn("prev");
+                }
+
+                document.getElementById("music").remove();
+                this.createAudio(
+                    (
+                        "http://localhost/audioPlayer/music/" +
+                        data[this.currentSong - 1]
+                    )
+                        .split(" ")
+                        .join("%20"),
+                    data[this.currentSong - 1]
+                );
+
+                this.getCurrentSongDuration();
+
+                if (this.playing) {
+                    document.getElementById("music").play();
+                }
+                this.currentSong--;
+            });
+    }
+
+    getCurrentSongDuration() {
         document.getElementById("music").onloadedmetadata = () => {
-            this.currentSongDuration = document.getElementById("music").duration
+            this.currentSongDuration =
+                document.getElementById("music").duration;
         };
     }
 
-    drawLine(){
-        let line = document.getElementById("line")
-        let context = line.getContext("2d")
-        let currentTime = document.getElementById("music").currentTime
-        let end = currentTime * line.width / this.currentSongDuration
+    drawLine() {
+        let line = document.getElementById("line");
+        let context = line.getContext("2d");
+        let currentTime = document.getElementById("music").currentTime;
+        let end = (currentTime * line.width) / this.currentSongDuration;
 
-        this.clearLine(line)
-        
-        context.moveTo(0, line.height/2)
-        context.lineTo(end, line.height/2)
-        context.lineWidth = 3
-        context.strokeStyle = "white"
-        context.stroke()    
+        this.clearLine(line);
+
+        context.moveTo(0, line.height / 2);
+        context.lineTo(end, line.height / 2);
+        context.lineWidth = 3;
+        context.strokeStyle = "white";
+        context.stroke();
     }
 
-    clearLine(line){
+    clearLine(line) {
         let w = line.width;
         line.width = 1;
         line.width = w;
     }
 
-    loop(){
-        let line = document.getElementById("line")
-        requestAnimationFrame(()=>this.loop())
-        this.drawLine()
-        if(document.getElementById("music").currentTime === this.currentSongDuration && !document.getElementById("next").disabled ){
-            this.nextSong()
+    loop() {
+        let line = document.getElementById("line");
+        requestAnimationFrame(() => this.loop());
+        this.drawLine();
+        if (
+            document.getElementById("music").currentTime ===
+                this.currentSongDuration &&
+            !document.getElementById("next").disabled
+        ) {
+            this.nextSong();
         }
     }
 
-    selectTime(){
-        let line = document.getElementById("line")
+    selectTime() {
+        let line = document.getElementById("line");
 
-        line.onmousedown = (e) => {    
-            let checkedTime = (e.pageX - line.offsetLeft) / line.width 
-            document.getElementById("music").currentTime = this.currentSongDuration * checkedTime     
-            this.mouseDown = true
-        }
+        line.onmousedown = (e) => {
+            let checkedTime = (e.pageX - line.offsetLeft) / line.width;
+            document.getElementById("music").currentTime =
+                this.currentSongDuration * checkedTime;
+            this.mouseDown = true;
+        };
 
         document.getElementById("songInfo").onmousemove = (event) => {
-            if(this.mouseDown){
-                let checkedTime = (event.pageX - line.offsetLeft) / line.width 
-                document.getElementById("music").currentTime = this.currentSongDuration * checkedTime 
+            if (this.mouseDown) {
+                let checkedTime = (event.pageX - line.offsetLeft) / line.width;
+                document.getElementById("music").currentTime =
+                    this.currentSongDuration * checkedTime;
             }
-        }
+        };
 
-        document.getElementById("songInfo").onmouseup = ()=>{
-            this.mouseDown=false
-        }
-    }
-    
-    disableBtn(id){
-        let btn = document.getElementById(id)
-        btn.disabled = true
-        btn.style.color = "#2121216c"
+        document.getElementById("songInfo").onmouseup = () => {
+            this.mouseDown = false;
+        };
     }
 
-    enableBtn(id){
-        let btn = document.getElementById(id)
-        btn.disabled = false
-        btn.style.color = "#5c5c5c"
+    disableBtn(id) {
+        let btn = document.getElementById(id);
+        btn.disabled = true;
+        btn.style.color = "#2121216c";
     }
 
-    onload =()=>{
-        this.createAudio(("./music/3.33 - TIKNIkneR.mp3"),"3.33 - TIKNIkneR.mp3") 
+    enableBtn(id) {
+        let btn = document.getElementById(id);
+        btn.disabled = false;
+        btn.style.color = "#5c5c5c";
+    }
+
+    onload = () => {
+        this.createAudio(
+            "./music/3.33 - TIKNIkneR.mp3",
+            "3.33 - TIKNIkneR.mp3"
+        );
         document.getElementById("music").onloadedmetadata = () => {
-            this.currentSongDuration = document.getElementById("music").duration
-        };   
-        this.drawLine()
-        this.selectTime()
-        this.loop()
-        this.disableBtn("prev")
-    }
+            this.currentSongDuration =
+                document.getElementById("music").duration;
+        };
+        this.drawLine();
+        this.selectTime();
+        this.loop();
+        this.disableBtn("prev");
+    };
 }
 
-let player = new Player
+let player = new Player();
 // player.onload()
+console.log(player.playPause());
 
 
-let a = new Buttons(player.playPause,player.playPause,player.prevSong,player.nextSong)
-a.createButtons()
-console.log(a)
-new Player(document.getElementById('myPlayer'))
-new Player(document.getElementById('myPlayer1'))
+new Player(document.getElementById("myPlayer"));
+new Player(document.getElementById("myPlayer1"));
